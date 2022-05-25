8ce86dbc953937b549f46557b834855dfa01cb6b
Ayush Garg <ayush181000@gmail.com>
2022-05-26 01:03:59 +0530

    Create Ques8.java

---
+++ Strings/Ques8.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Ques8 {
+
+}
+
+class Solution {
+
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        for (String s : path.split("/")) {
+            if (s.equals("..")) {
+                if (stack.size() > 0)
+                    stack.pop();
+            } else if (!s.equals("") && !s.equals(".")) {
+                stack.push(s);
+            }
+        }
+
+        if (stack.size() == 0) {
+            return "/";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (stack.size() > 0) {
+            sb.append("/").append(stack.pollLast());
+        }
+        return sb.toString();
+    }
+}
